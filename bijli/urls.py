27e6db609a8c6313ab2d775fa4a6548ad8b6c42e
abhishekk1004 <abhishekkushwaha.np@gmail.com>
2026-07-06 17:29:26 +0545

    Serve uploaded media in production

--- bijli/urls.py
+++ bijli/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 from apps.core.sitemaps import SITEMAPS
 from apps.core.views import robots_txt
@@ -21,10 +21,10 @@ urlpatterns = [
     path('gallery/', include('apps.gallery.urls', namespace='gallery')),
     path('contact/', include('apps.contact.urls', namespace='contact')),
     path('api/', include('apps.api.urls', namespace='api')),
-]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+   
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
 handler403 = 'apps.core.views.handler403'
 handler404 = 'apps.core.views.handler404'
